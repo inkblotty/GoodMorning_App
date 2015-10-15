09045b97a57ba39a73103052aa2b4b0504d4477d
inkblotty <inkblot.and.blush@gmail.com>
2015-10-15 11:49:55 -0600

    updates to index and weathCtrl

--- js/controllers/weathCtrl.js
+++ js/controllers/weathCtrl.js
@@ -6,7 +6,5 @@ app.controller('weathCtrl', ['$scope', 'weathService', function($scope, weathSer
 	weathService.success(function(data) {
 		weath.weathData = data.current_observation;
 		weath.location = data.current_observation.display_location;
-		//console.log(weath.weathData);
-		//console.log(weath.location);
 	});
 }]);
